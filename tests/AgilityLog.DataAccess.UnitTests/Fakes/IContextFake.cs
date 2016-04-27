51802845bffc49c5bc524e0b7cccde004996e206
MarkDD <mark.dendunnen@gmail.com>
2016-04-27 10:36:04 +0100

    commit from home

---
+++ tests/AgilityLog.DataAccess.UnitTests/Fakes/IContextFake.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AgilityLog.DataAccess.EntityRepository;
+using Microsoft.Data.Entity;
+
+namespace AgilityLog.DataAccess.UnitTests.Fakes
+{
+    public class IContextFake<T> : IContext
+        where T : class
+    {
+        private readonly Func<DbSet<T>> entitySetFunc;
+
+        public IContextFake(Func<DbSet<T>> entitySetFunction)
+        {
+            this.entitySetFunc = entitySetFunction;
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DbSet<TEntity> EntitySet<TEntity>() where TEntity : class
+        {
+            return this.entitySetFunc.Invoke() as DbSet<TEntity>;
+        }
+    }
+}
